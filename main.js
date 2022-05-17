71762acfa599155f614a475f8c8dce9517ea9062
Filip Škoro <f.skoro06@gmail.com>
2022-05-17 16:23:45 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//prevedi na engleski
+	function prevediEng() {
+		document.getElementById("pocetnaLink").innerHTML = "Home";
+		document.getElementById("putovnicaLink").innerHTML = "Croatian cultural passport";
+		document.getElementById("kruznaLink").innerHTML = "Circular creativity";
+		document.getElementById("kontaktLink").innerHTML = "Contact";
+		document.getElementById("vilijunNaslov").innerHTML = "VILION";
+		document.getElementById("vilijunParaf").innerHTML = "Villion is a novel intended for those who are inclined to travel and get to know themselves through the eyes of another";
+		document.getElementById("pocetna").style.backgroundImage = "url(assets/pozadina/Villion-1.png)";
+		document.getElementById('logo-img').src='assets/logo/Logo_Millenial.png';
+	}
+
+//prevedi na hrvatski
+	function prevediHrv() {
+		document.getElementById("pocetnaLink").innerHTML = "Početna";
+		document.getElementById("putovnicaLink").innerHTML = "Hrvatska kulturna putovnica";
+		document.getElementById("kruznaLink").innerHTML = "Kružna kreativnost";
+		document.getElementById("kontaktLink").innerHTML = "Kontakt";
+		document.getElementById("vilijunNaslov").innerHTML = "VILIJUN";
+		document.getElementById("vilijunParaf").innerHTML = "Vilijun je roman namijenjen onima koji su skloni putovanjima i upoznavanju samoga sebe očima drugoga";
+		document.getElementById("pocetna").style.backgroundImage = "url(assets/pozadina/pozadina4.jpg)";
+		document.getElementById('logo-img').src='assets/logo/Logo_Milenijsko.png';
+	}
+
+//prevedi na kineski
+	function prevediKin() {
+		document.getElementById("pocetnaLink").innerHTML = "主页zhǔyè";
+		document.getElementById("putovnicaLink").innerHTML = "克罗地亚文化护照";
+		document.getElementById("kruznaLink").innerHTML = "循环创造力";
+		document.getElementById("kontaktLink").innerHTML = "联系方式";
+		document.getElementById("vilijunNaslov").innerHTML = "吾临";
+		document.getElementById("vilijunParaf").innerHTML = "吾临 是一本为想要通过世界和旅行聊了解自己的人而写的小说。";
+		document.getElementById("pocetna").style.backgroundImage = "url(assets/pozadina/Unknown.jpg)";
+		document.getElementById('logo-img').src='assets/logo/Logo_Milenijsko_Kineski.png';
+	}
